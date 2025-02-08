6764f538fbb5c9afe2fb40aefd57e754fbcafd73
Kavini Weerasinghe <kavini.w.weerasinghe@gmail.com>
2025-02-08 10:29:23 +0530

    sort five

--- src/sort.c
+++ src/sort.c
@@ -12,37 +12,6 @@
 
 #include "../includes/push_swap.h"
 
-static int	is_min(int cmp, t_stack *stack)
-{
-	int	*arr;
-	int	len;
-
-	arr = stack -> a;
-	len = stack -> len_a;
-	while (len--)
-	{
-		if (*arr < cmp)
-			return (0);
-		arr++;
-	}
-	return(1);
-}
-
-static int	is_max(int cmp, t_stack *stack)
-{
-	int	*arr;
-
-	arr = stack -> a;
-	while (*arr)
-	{
-		if (*arr > cmp)
-			return (0);
-		arr++;
-	}
-	return(1);
-}
-
-
 static void	sort_three(int *ar, t_stack *stack)
 {
 	int	i;
@@ -70,27 +39,19 @@ void	simple_sort(int len, t_stack *stack)
 		return (sa(stack));
 	while (stack -> len_a > 3)
 	{
-		if (is_min(*stack -> a, stack) || is_max(*stack -> a, stack))
-		{
-			printf("stack -> a = %i\n" , *stack -> a);
+		if (*stack -> a == 0 || *stack -> a == 1) 
 			pb(stack);
-		}
-		else
+		else if (stack -> a[stack -> len_a - 1] == 0
+			|| stack -> a[stack -> len_a - 1] == 1)
+			rra(stack);
+		else 
 			ra(stack);
 	}
 	sort_three(stack -> a, stack);
 	while(stack -> len_b)
-	{
 		pa(stack);
-		if (stack -> len_b && *stack -> a > *(stack -> a + 1) 
-			&& !(*stack -> a > *stack -> b
-			&& *stack -> b > *(stack -> a + 1)))
-			ra(stack);
-	}
 	if (*stack -> a > *(stack -> a + 1))
 		sa(stack);
-	while (!is_min(*stack -> a, stack))
-		ra(stack);
 }
 
 void	radix_sort(int iter, int bit_mask, int len, t_stack *stack)
